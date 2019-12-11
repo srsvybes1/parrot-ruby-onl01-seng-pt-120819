025f7661cc13fc052d009f1427f0f9e3064e1495
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-11 06:36:21 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,7 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot (bird = "Squawk!")
+  puts bird
+  return bird
+    end
+  
+  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
