56ccfda9867773a325bfd655ae616f585f376edb
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-11 06:32:50 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot 
+  bird = "Squawk!"
+  puts "Squawk!"
+  return bird
+  
+  end
+  
+  # Create method `parrot` that outputs a given phrase and
 # returns the phrase
